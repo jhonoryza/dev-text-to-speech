5e17bf1abb253a0c6ff805e1c3217e572a94350c
fajar sp <jardik.oryza@gmail.com>
2016-01-25 10:09:13 +0000

    tiga

--- textspeech.js
+++ textspeech.js
@@ -1,5 +1,4 @@
-/* global describe it before */
-
+/* globals jquery lodash someOtherLibrary */
 var input, button, greeting;
 var buttonTest, buttonListVoices, buttonSave;
 var foo;
@@ -12,6 +11,8 @@ var downloadMp3;
 var inputName;
 var downloadFile;
 var selectKorNum, selectHalNum;
+var buttonKorNum, buttonHalNum;
+
 function setup() {
   //createCanvas(710, 400);
   // foo = new p5.Speech(); // speech synthesis object
@@ -19,119 +20,78 @@ function setup() {
   //foo.setVoice('native');
 
   //fill(150);
-  stroke(0);
+  //stroke(0);
   noCanvas();
-
-  greeting = createElement('h2', 'speak it?');
+  
+  greeting = createElement('h2', 'text to speech');
   greeting.position(20, 5);
-
+  greeting.class('lemon');
   input = createInput();
   input.position(20, 65);
-
   button = createButton('play');
   button.position(200, 65);
   button.mousePressed(greet);
-
   downloadFile = createA('#', 'save as mp3');
   downloadFile.position(270, 65);
+  downloadFile.class('link');
 
-  selectKorNum = createSelect();
-  selectKorNum.position(20, 90);
-  
-  // buttonSave = createButton('save');
-  // buttonSave.position(250,65);
-  // buttonSave.mousePressed(goUrl);
-
-  // buttonTest = createButton('p5.speech.js test api');
-  // buttonTest.position(150,100);
-  // buttonTest.mousePressed(go);
-
-  // buttonListVoices = createButton('list voices');
-  // buttonListVoices.position(150,130);
-  // buttonListVoices.mousePressed(goo);
-
-  // buttonUrl = createButton('url test');
-  // buttonUrl.position(150,170);
-  // buttonUrl.mousePressed(goUrl);
-
-
-  // textAlign(CENTER)
-  // textSize(50);
-
-  //downloadMp3 = document.getElementById('where_to_insert');
-  //downloadMp3 = select('id','where_to_insert');
-  //downloadMp3.position(100,100);
-
-  // downloadMp3.innerHTML = "";
-  // downloadMp3.href = "https://code.responsivevoice.org/getvoice.php?t=selamat%20datang&tl=id&sv=&vn=&pitch=0.5&rate=0.5&vol=1";
-  // downloadMp3.download = "play";
+  var textKorNum = createElement('span', 'jumlah koridor'); 
+  textKorNum.class('lemon');
+  textKorNum.position(20, 120);
+  selectKorNum = createInput();
+  selectKorNum.position(130, 120);
+  var textKorHal = createSpan('jumlah halte'); 
+  textKorHal.class('lemon');
+  textKorHal.position(20, 150);
+  selectHalNum = createInput();
+  selectHalNum.position(130, 150);
+  buttonKorNum = createButton('create');
+  buttonKorNum.position(290, 150);
+  buttonKorNum.mousePressed(createList);
 }
 
 function draw() {
   background(255);
 }
-
-function go() {
-  foo.speak('Selamat Datang di Kota Bandung'); // say something
-}
-
-function goo() {
-  foo.listVoices();
+var me = [];
+function createList(){
+  var numKor = selectKorNum.value();
+  var numHal = selectHalNum.value();
+  selectKorNum.value('');
+  selectHalNum.value('');
+  
+  
+  for(var i=1; i<=me.length; i++){
+    if(me[i] != null)
+      me[i].remove();
+  }
+  for(var i=1; i<=numKor; i++){
+    me[i] = createElement('Div', 'koridor ' +i);
+    me[i].class('lemon');
+    me[i].position(20, 180+(i*30));
+  }
+  
+  for(var i=1; i<=numHal; i++){
+    me[i] = createInput();
+    me[i].position(20, 'auto');
+  }
 }
 
 function greet() {
   responsiveVoice.speak(input.value(), "Indonesian Female", {
     onend: greetFinished
   });
-  var name = input.value();
-  greeting.html('hello ' + name + '!');
 }
 
 function greetFinished() {
   console.log("voice generated");
   var name = input.value();
+  input.value('');
   name = name.replace(/\s/g, "%20");
+  input.value = '';
   console.log(name);
   url = "https://responsivevoice.org/responsivevoice/getvoice.php?t=" + name + "&tl=id&sv=&vn=&pitch=0.5&rate=0.5&vol=1";
   console.log(url);
-  //downloadMp3.attribute("href", url);
-  //downloadMp3.attribute("html", "click here");
-  // downloadMp3.href = url;
-  // downloadMp3.title = "to mp3";
-  // downloadMp3.innerHTML = "save as mp3";
-  // downloadMp3.download = "myvoice.mp3";
   downloadFile.attribute("href", url);
   downloadFile.attribute("html", "save as mp3");
-}
-
-function goUrl() {
-  var name = input.value();
-  name = name.replace(/\s/g, "%20");
-  console.log(name);
-  // url = "https://code.responsivevoice.org/getvoice.php?t=" +name +"&tl=id&sv=&vn=&pitch=0.5&rate=0.5&vol=1";
-  url = "https://responsivevoice.org/responsivevoice/getvoice.php?t=" + name + "&tl=id&sv=&vn=&pitch=0.5&rate=0.5&vol=1";
-  console.log(url);
-  httpGet(url, finished);
-}
-
-function changeUrl() {
-
-  downloadMp3.style.position = "relative";
-  downloadMp3.href = url;
-  downloadMp3.title = "download as mp3";
-  downloadMp3.innerHTML = "save as mp3";
-  downloadMp3.download = "myvoice.mp3";
-  downloadMp3.click();
-
-  // var testLink = createA('url','or click here');
-  // testLink.prototype.replace("href", url);
-}
-
-function mee() {
-  save(link, 'ke.mp3');
-}
-
-function finished(response) {
-  console.log(response);
-  changeUrl();
 }
