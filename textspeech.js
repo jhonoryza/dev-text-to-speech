c8c6c5421a73338ae4c97458cda9251bfb52e013
fajar sp <jardik.oryza@gmail.com>
2016-01-27 05:03:41 +0000

    5

--- textspeech.js
+++ textspeech.js
@@ -12,7 +12,7 @@ var inputName;
 var downloadFile;
 var selectKorNum, selectHalNum;
 var buttonKorNum, buttonHalNum;
-
+var inputinputPitch, inputinputRate, inputinputVolume;
 function setup() {
   noCanvas();
   var head = createElement('h1', 'hello world')
@@ -25,19 +25,35 @@ function setup() {
   greeting.style('margin', '0px 47px 0px 5px');
   greeting.class('textBox');
   input = createInput();
+  input.value('selamat datang');
   input.class('inputBox');
+  input.changed(greet);
   button = createButton('play');
   button.class('tombol');
   button.mousePressed(greet);
   downloadFile = createA('#', 'save as mp3');
   downloadFile.class('link');
-  var nl = createElement('br');
+  var textinputPitch = createElement('t', 'pitch (0 - 2)');
+  textinputPitch.class('textBox');
+  inputPitch = createInput();
+  inputPitch.value('0.9');
+  inputPitch.class('inputBox');
+  var textinputRate = createElement('t', 'rate (0 - 1.5)');
+  textinputRate.class('textBox');
+  inputRate = createInput();
+  inputRate.class('inputBox');
+  inputRate.value('1');
+  var textinputVolume = createElement('t', 'volume (0 - 1)');
+  textinputVolume.class('textBox');
+  inputVolume = createInput();
+  inputVolume.class('inputBox');
+  inputVolume.value('1');
   var textKorNum = createElement('t', 'jumlah koridor'); 
-  textKorNum.style('margin', '0px 10px 0px 5px');
+  textKorNum.style('margin', '0px 13px 0px 5px');
   textKorNum.class('textBox');
   selectKorNum = createInput();
   selectKorNum.class('inputBox');
-  selectKorNum.changed(checkInput);
+  selectKorNum.changed(createList);
   buttonKorNum = createButton('create');
   buttonKorNum.class('tombol');
   buttonKorNum.mousePressed(createList);
@@ -46,72 +62,86 @@ function setup() {
   inputUser.child(input);
   inputUser.child(button);
   inputUser.child(downloadFile);
-  inputUser.child(nl);
+  inputUser.child(textinputPitch);
+  inputUser.child(inputPitch);
+  inputUser.child(textinputRate);
+  inputUser.child(inputRate);
+  inputUser.child(textinputVolume);
+  inputUser.child(inputVolume);
+  inputUser.child(createElement('br'));
   inputUser.child(textKorNum);
   inputUser.child(selectKorNum);
   inputUser.child(buttonKorNum);
   
 }
-function checkInput(){
-  var data = selectKorNum.value();
-  if(isNaN(data)){
+
+function draw() {
+  background(255);
+}
+
+var me = [], textTitle = [], textDesc = [], inputHal = [], textDescP = [], inputHalP = [];
+function createList(){
+  var numKor = selectKorNum.value();
+  if(isNaN(numKor)){
     //not number
     alert("fill in number");
   }
   else{
     //its number
     //alert("correct");
-  }
-}
-function draw() {
-  background(255);
-}
-var me = [];
-var textTitle = [];
-var inputHal = [];
-var buttonHal = [];
-function createList(){
-  var numKor = selectKorNum.value();
-
-  for(var i=1; i<=me.length; i++){
-    if(me[i] != null)
-      me[i].remove();
-  }
-  
-  for(var i=1; i<=numKor; i++){
-    me[i] = createElement('div');
-    me[i].class('box');
+    //hapus div
+    for(var i=1; i<=me.length; i++){
+      if(me[i] != null)
+        me[i].remove();
+    }
     
-    textTitle[i] = createElement('p', 'koridor ' +i);
-    textTitle[i].class('title');
-    
-    inputHal[i] = createElement('input');
-    inputHal[i].class('inputBox');
-    buttonHal[i] = createElement('button', 'create');
-    buttonHal[i].class('tombol');
-    
-    me[i].child(textTitle[i]);
-    me[i].child(inputHal[i]);
-    me[i].child(buttonHal[i]);
+    //create div
+    for(var i=1; i<=numKor; i++){
+      me[i] = createElement('div');
+      me[i].class('box');
+      
+      textTitle[i] = createElement('h2', 'koridor ' +i);
+      textTitle[i].class('title');
+      textDesc[i] = createElement('t', 'input halte (jalur berangkat) :');
+      textDesc[i].style('margin','0px 0px 0px 10px');
+      textDesc[i].class('textBox');
+      
+      inputHal[i] = createElement('textarea');
+      inputHal[i].class('inputBoxHalte');
+      textDescP[i] = createElement('t', 'input halte (jalur pulang) :');
+      textDescP[i].style('margin','0px 0px 0px 10px');
+      textDescP[i].class('textBox');
+      inputHalP[i] = createElement('textarea');
+      inputHalP[i].class('inputBoxHalte');
+      
+      me[i].child(textTitle[i]);
+      me[i].child(textDesc[i]);
+      me[i].child(createElement('br'));
+      me[i].child(inputHal[i]);
+      me[i].child(createElement('br'));
+      me[i].child(textDescP[i]);
+      me[i].child(createElement('br'));
+      me[i].child(inputHalP[i]);
+    }
   }
-
 }
 
 function greet() {
-  responsiveVoice.speak(input.value(), "Indonesian Female", {
-    onend: greetFinished
-  });
+  var p = inputPitch.value(); var r = inputRate.value(); var v = inputVolume.value(); var i = input.value();
+  //console.log(p +" " +r +" " +v);
+  responsiveVoice.speak(i, "Indonesian Female", {pitch: p, rate: r, volume: v, onend: greetFinished});
+  //responsiveVoice.speak(input.value(), "Indonesian Female", {pitch: p});
+  //responsiveVoice.speak(input.value(), "Indonesian Female", {onend: greetFinished});
 }
 
 function greetFinished() {
   console.log("voice generated");
   var name = input.value();
-  input.value('');
   name = name.replace(/\s/g, "%20");
-  input.value = '';
-  console.log(name);
-  url = "https://responsivevoice.org/responsivevoice/getvoice.php?t=" + name + "&tl=id&sv=&vn=&pitch=0.5&rate=0.5&vol=1";
+  //url = "https://responsivevoice.org/responsivevoice/getvoice.php?t=" + name + "&tl=id&sv=&vn=&pitch=" +inputPitch.value() +"&rate=" +inputRate.value() +"&vol=" +inputVolume.value();
+  url = responsiveVoice.src;
   console.log(url);
   downloadFile.attribute("href", url);
   downloadFile.attribute("html", "save as mp3");
 }
+
